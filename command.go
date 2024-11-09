9864f4aa0191f4abd54cd7d21958e18e6e20d49f
Vladislav <warzon131@gmail.com>
2024-11-09 07:27:58 +0200

    add handlers

--- command.go
+++ command.go
@@ -5,13 +5,30 @@ import (
 	"gator/internal/config"
 )
 
-type state struct {
-	cfg *config.Config
+type commands struct {
+	com map[string]func(*state, command) error
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.com[name] = f
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	err := c.com[cmd.name](s, cmd)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 type command struct {
 	name    string
 	handler []string
+}
+
+type state struct {
+	cfg *config.Config
 }
 
 func handlerLogin(s *state, cmd command) error {
--- main.go
+++ main.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	"gator/internal/config"
+	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Not enough arguments")
+		os.Exit(1)
+	}
+
 	c, _ := config.Read()
-	c.SetUser("Vlad") // login
-	c, _ = config.Read()
-	fmt.Println(c)
+	s := state{&c}
+
+	com := commands{make(map[string]func(*state, command) error)}
+	com.register("login", handlerLogin)
+
+	pass := command{args[0], args[1:]}
+
+	err := com.run(&s, pass)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
