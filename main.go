8993e5044ed6673afd3fb67718c3453767880fad
Vladislav <warzon131@gmail.com>
2024-11-09 08:05:06 +0200

    refactor main

--- main.go
+++ main.go
@@ -13,15 +13,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	c, _ := config.Read()
-	s := state{&c}
+	cfg, err := config.Read()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	s := state{&cfg}
 
 	com := commands{make(map[string]func(*state, command) error)}
 	com.register("login", handlerLogin)
 
-	pass := command{args[0], args[1:]}
-
-	err := com.run(&s, pass)
+	err = com.run(&s, command{args[0], args[1:]})
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
