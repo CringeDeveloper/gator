ee0f153449f9cec263c394f0c1d50d59778c0969
Vladislav <warzon131@gmail.com>
2024-11-11 11:34:33 +0200

    add feeds migration

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+CREATE TABLE feeds(
+    id uuid PRIMARY KEY,
+    created_at timestamp NOT NULL,
+    updated_at timestamp NOT NULL,
+    name text,
+    url text NOT NULL UNIQUE,
+    user_id uuid NOT NULL REFERENCES users ON DELETE CASCADE,
+    CONSTRAINT  fk_user
+        FOREIGN KEY (user_id)
+            REFERENCES users(id)
+);
+
+-- +goose Down
+DROP TABLE feeds;
